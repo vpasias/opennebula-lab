d568bcb85168e6367b45addc750341891db5f953
vpasias <52623837+vpasias@users.noreply.github.com>
2022-05-28 11:36:10 +0300

    Update bootstrap_compute.sh

--- bootstrap_compute.sh
+++ bootstrap_compute.sh
@@ -4,7 +4,7 @@ set -e
 sudo apt-get update
 sudo apt-get install -y gnupg curl wget apt-transport-https
 
-sudo -- sh -c 'echo "deb https://downloads.opennebula.io/repo/6.1/Debian/11 stable opennebula" > /etc/apt/sources.list.d/opennebula.list'
+sudo -- sh -c 'echo "deb https://downloads.opennebula.io/repo/6.4/Debian/11 stable opennebula" > /etc/apt/sources.list.d/opennebula.list'
 sudo -- sh -c 'wget -q -O- https://downloads.opennebula.io/repo/repo.key | apt-key add -'
 sudo apt-get update
 sudo apt-get install -y opennebula-node-kvm 
